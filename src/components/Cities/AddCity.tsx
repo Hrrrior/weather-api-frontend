d7a72eb380801758d795460f696757a632beebf1
heheli <henri.helisma@outlook.com>
2022-05-15 03:09:16 +0300

    small update

--- src/components/Cities/AddCity.tsx
+++ src/components/Cities/AddCity.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { ICity } from '../../services/dto/ICity';
 import classes from './City.module.css';
